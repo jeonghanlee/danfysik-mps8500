de3f2ed2332c1484cf5290d4b442f757ec8333ce
Jeong Han Lee <jeonghan.lee@gmail.com>
2016-11-28 16:58:42 +0100

    move terminators into st.cmds

--- iocBoot/iocmps8500/st.cmd
+++ iocBoot/iocmps8500/st.cmd
@@ -1,7 +1,9 @@
 #!../../bin/linux-x86_64/mps8500
 
-## You may have to change mps8500 to something else
-## everywhere it appears in this file
+#  author : Jeong Han Lee
+#  email  : jeonghan.lee@gmail.com
+#  Date   : 
+#  version : 0.0.1
 
 < envPaths
 
@@ -11,11 +13,29 @@ cd "${TOP}"
 dbLoadDatabase "dbd/mps8500.dbd"
 mps8500_registerRecordDeviceDriver pdbbase
 
+
+# We will use the MOXA NPort 6650-8 
+drvAsynIPPortConfigure "MPS8500", "127.0.0.1:9999", 0, 0, 0
+asynOctetSetInputEos   "MPS8500", 0, "\n\r"
+asynOctetSetOutputEos  "MPS8500", 0, "\r"
+
+#
+# "\r\n" is only valid for the simulator
+# , because we want to use Telnet connection also.
+#
+# asynOctetSetOutputEos  "MPS8500", 0, "\r\n"
+
+
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=jhlee")
+dbLoadRecords "db/iocAdminSoft.db",  "IOC=8500:IocStat"
+dbLoadRecords "db/mps8500.db",       "SYSDEV=8500:MPS,INST=1,MAX=100,MIN=0,HWUNIT=MPS8500"
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
 ## Start any sequence programs
 #seq sncxxx,"user=jhlee"
+
+
+dbl > "${TOP}/${IOC}_PVs.list"
+
